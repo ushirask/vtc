a12a3fdab769926f9efd6f6f0a1e4a78d3d30781
Saranya-Uthayakumar <36379440+Saranya-Uthayakumar@users.noreply.github.com>
2018-04-17 20:51:56 +0530

    updated lecturer profile page

---
+++ inc/leaveformlecturerdb.inc.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$conn = mysqli_connect('localhost', 'root', '', 'lecturer');
+$id=$_SESSION['login_user'];
+$num=mysqli_real_escape_string($conn,$_POST['number']);
+$dfrom=mysqli_real_escape_string($conn,$_POST['from']);
+$dto=mysqli_real_escape_string($conn,$_POST['to']);
+$reason=mysqli_real_escape_string($conn,$_POST['reason']);
+
+if(isset($_POST['submit'])){
+	$sql="INSERT INTO lecturer_leave (id,num,dfrom, dto, reason) VALUES ('$id',  '$num', '$dfrom', '$dto', '$reason');";
+	mysqli_query($conn, $sql);
+	header("Location: ../teacher-profile.php?successfull");
+}
