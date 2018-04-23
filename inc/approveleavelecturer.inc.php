cabb37db0ce42aeecd006fd063f11fd5a0b14278
Saranya-Uthayakumar <36379440+Saranya-Uthayakumar@users.noreply.github.com>
2018-04-23 18:24:59 +0530

    corrected errors

--- inc/approveleavelecturer.inc.php
+++ inc/approveleavelecturer.inc.php
@@ -3,12 +3,12 @@ $conn=mysqli_connect('localhost','root','','vtc');
 $stuid=$_GET['stuid'];
 if (isset($_POST['approve'])) {
 	$data="Approved";
-	$sql1="UPDATE student_leave SET lecturerApproval = '$data' WHERE indexnumber='$stuid';";
+	$sql1="UPDATE student_leave SET sectionalHeadApproval = '$data' WHERE indexnumber='$stuid';";
 	mysqli_query($conn,$sql1);
 	header("Location: ../leave-application-list.php?approved");
 }else if(isset($_POST['reject'])){
 	$data="Rejected";
-	$sql1="UPDATE student_leave SET lecturerApproval = '$data' WHERE indexnumber='$stuid';";
+	$sql1="UPDATE student_leave SET sectionalHeadApproval = '$data' WHERE indexnumber='$stuid';";
 	mysqli_query($conn,$sql1);
 	header("Location: ../leave-application-list.php?rejected");
 }
--- inc/leaveformdb.inc.php
+++ inc/leaveformdb.inc.php
@@ -3,14 +3,14 @@ session_start();
 $conn = mysqli_connect('localhost', 'root', '', 'vtc');
 $indexnumber=$_SESSION['login_user'];
 $course=mysqli_real_escape_string($conn,$_POST['course']);
-$moduleCode=mysqli_real_escape_string($conn,$_POST['moduleCode']);
 $num=mysqli_real_escape_string($conn,$_POST['number']);
 $dfrom=mysqli_real_escape_string($conn,$_POST['from']);
 $dto=mysqli_real_escape_string($conn,$_POST['to']);
 $reason=mysqli_real_escape_string($conn,$_POST['reason']);
+$sectionalHeadApproval="awaiting";
 
 if(isset($_POST['submit'])){
-	$sql="INSERT INTO student_leave (indexnumber,course,moduleCode,num,dfrom, dto, reason) VALUES ('$indexnumber', '$course', '$moduleCode', '$num', '$dfrom', '$dto', '$reason');";
+	$sql="INSERT INTO student_leave (indexnumber,course,num,dfrom, dto, reason, sectionalHeadApproval) VALUES ('$indexnumber', '$course', '$num', '$dfrom', '$dto', '$reason','$sectionalHeadApproval');";
 	mysqli_query($conn, $sql);
 	header("Location: ../profile.php?successfull");
 }
