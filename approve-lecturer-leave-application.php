fe0a1973158f6111ed198a91656818b6aa442934
Saranya-Uthayakumar <36379440+Saranya-Uthayakumar@users.noreply.github.com>
2018-04-23 18:29:02 +0530

    updated principal profile

---
+++ approve-lecturer-leave-application.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Learn Center | Approve Leave Application</title>
+<?php include('inc/header.inc.php'); ?>
+<div class="body2">
+  <div class="main">
+    <!-- content -->
+    <?php $lecid=$_GET['lid'];?>
+		  <div class="container">
+			<?php include('inc/connectdb1.inc.php');?>
+			<div class="clearfix">
+				<form style="border:1px solid #ccc" method ='POST' action="inc/approveleaveprincipal.inc.php?lecid=<?php echo $lecid;?> ">
+				<label for="number"><b>Number of Days</b></label><br><br>
+				<input type="number" value="<?php echo $num ; ?>" readonly><br><br>
+				
+				<label for="from"><b>Period of leave<br><t>  From</b></label><br>
+				<input type="date" value="<?php echo $dfrom ; ?>" name="from" min="2015-01-01" max="2035-12-31" readonly><br>
+
+				<label for="to"><b><t>  To</b></label><br>
+				<input type="date" value="<?php echo $dto ; ?>" name="to" min="2015-01-01" max="2035-12-31" ><br><br>
+
+				<label for="reason"><b>Reason</b></label><br>
+			  <button name= "approve" type="submit" class="signupbtn" name="apply" >Approve</button><br>
+			  <button name= "reject" type="submit" class="signupbtn" name="apply" >Reject</button>
+			</div>
+		  </div>
+		</form>
+		
+    <!-- content -->
+    <?php include('inc/footer.inc.php'); ?>
+  </div>
+</div>
+<script type="text/javascript">Cufon.now();</script>
+</body>
+</html>
---
+++ principal-profile.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Learn Center</title>
+<?php include('inc/header.inc.php'); ?>
+<div class="body2">
+  <div class="main">
+    <!-- content -->
+    <section id="content">
+      <div class="wrapper">
+        <div class="pad1 pad_top1">
+          <article class="cols marg_right1">
+            <figure><a href="#"><img src="images/page1_img1.jpg" alt=""></a></figure>
+            <span class="font1">Our Mission Statement</span> </article>
+          <article class="cols marg_right1">
+            <figure><a href="#"><img src="images/page1_img2.jpg" alt=""></a></figure>
+            <span class="font1">Performance Report</span> </article>
+          <article class="cols">
+            <figure><a href="#"><img src="images/page1_img3.jpg" alt=""></a></figure>
+            <span class="font1">Prospective Parents</span> </article>
+        </div>
+      </div>
+      <div class="box1">
+        <div class="wrapper">
+          <article class="col1">
+            <div class="pad_left1">
+              <h2>Welcome to Our Center</h2>
+              <p class="font2">Learn Center is one of free website templates created by <span>TemplateMonster.com team</span></p>
+              <p><strong>Learn Center Template</strong> is optimized for 1024X768 screen resolution. It’s also XHTML &amp; CSS valid. This website template has several pages: <a href="courses.html">Courses</a>, <a href="programs.html">Programs</a>, <a href="teachers.html">Teachers</a>, <a href="admissions.html">Admissions</a>, <a href="contacts.html">Contacts</a> (note that contact us form – doesn’t work).</p>
+            </div>
+            <a href="#" class="button"><span><span>Read More</span></span></a>
+            <div class="pad_left1">
+              <h2>Individual Approach to Education!</h2>
+            </div>
+            <div class="wrapper">
+              <figure class="left marg_right1"><img src="images/page1_img4.jpg" alt=""></figure>
+              <p class="pad_bot1 pad_top2"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod tempor incididunt ut labore.</strong></p>
+              <p> Learn Center Template goes with two packages – with PSD source files and without them. PSD source files are available for free for the registered members of Templates.com. The basic package (without PSD source is available for anyone without registration).</p>
+            </div>
+            <div class="pad_top2"> <a href="#" class="button"><span><span>Read More</span></span></a> </div>
+          </article>
+          <article class="col2 pad_left2">
+            <div class="pad_left1">
+              <h2>New Programs</h2>
+            </div>
+            <ul class="list1">
+              <li><a href="lecturer-leave-application-list.php">Approve leave applications</a></li>
+            </ul>
+            <div class="pad_left1">
+              <h2>Latest News</h2>
+            </div>
+            <div class="wrapper"> <span class="date">27</span>
+              <p class="pad_top2"><a href="#">April, 2011</a><br>
+                Sed utirspiciatis unde omnis iste natus error sit...</p>
+            </div>
+            <div class="wrapper"> <span class="date">25</span>
+              <p class="pad_top2"><a href="#">April, 2011</a><br>
+                Voluptatem accusan dolore mque laudantium...</p>
+            </div>
+            <div class="pad_top2"> <a href="logout.php" class="button"><span><span>LOGOUT</span></span></a> </div>
+          </article>
+        </div>
+      </div>
+    </section>
+    <!-- content -->
+    <?php include('inc/footer.inc.php'); ?>
+  </div>
+</div>
+<script type="text/javascript">Cufon.now();</script>
+</body>
+</html>
