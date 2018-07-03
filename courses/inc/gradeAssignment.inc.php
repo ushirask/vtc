bea6fad63355d8aa0ee458e14dbe2d4426e2f988
Saranya-Uthayakumar <36379440+Saranya-Uthayakumar@users.noreply.github.com>
2018-07-03 22:29:07 +0530

    added submission of assignments

---
+++ courses/inc/gradeAssignment.inc.php
@@ -0,0 +1,12 @@
+<?php
+$assignId=$_GET['assignId'];
+$student_id=$_GET['stuId'];
+if(isset($_POST['grade'])){
+	$marks=$_POST['marks'];
+
+	$conn=mysqli_connect('localhost','root','','vtc');
+	$sql="INSERT INTO student_assignments (graded_status, marks) VALUES (TRUE, '$marks');";
+	mysqli_query($conn, $sql);
+	$result=mysqli_query($conn,$sql);
+	header("Location: ../assignment_view.php?successfull");
+}
